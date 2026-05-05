ac0f9a6d6f115875aa8a2d1811d8055e2b5f2cea
raflord <rafaelwrobel2000@gmail.com>
2026-05-04 22:03:10 -0300

    chore: removing unused imports

--- client/src/components/NavMain.tsx
+++ client/src/components/NavMain.tsx
@@ -1,6 +1,5 @@
-import { type Icon, IconCirclePlusFilled, IconMail } from "@tabler/icons-react";
+import type { Icon } from "@tabler/icons-react";
 
-import { Button } from "@/components/ui/button";
 import {
   SidebarGroup,
   SidebarGroupContent,
